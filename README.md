7f81e40582356c2d545b519eb0239c41985d7088
Chloride Cull <chloride@devurandom.net>
2022-01-11 19:21:34 +0100

    Initial import

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Unity Stuff
 Collection of models, textures, materials, prefabs, and so on, for both general Unity game making, modding, and VRChat.
 
-# License
+## License
 Except for C# and U# code or where otherwise noted the content of this repo is available under [Creative Commons Attribution 4.0 International](https://creativecommons.org/licenses/by/4.0/).
 
 Except otherwise noted C# and U# code is released under the [MIT License](https://choosealicense.com/licenses/mit/).
 
 I request to be credited as `Sebastian "Chloride Cull" Johansson` or `Sebastian "Dusty" Johansson`.
+
+## Using this repo
+Either download it as a zip off Github, or, if you want the ability to do updates, you can clone it with `git clone --filter=blob:none https://github.com/ChlorideCull/UnityStuff.git` to avoid having to download all files in the history of the repo.
+In the package manager, add one of the folders as a local package.
---
+++ VRCStuff/VRC Specific/Udon/CalibratedSeat.cs
@@ -0,0 +1,57 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CalibratedSeat : UdonSharpBehaviour
+{
+    public Transform seatEdge;
+    private VRC.SDK3.Components.VRCStation _station;
+    private Vector3 _initialPosition;
+    
+    void Start()
+    {
+        _station = (VRC.SDK3.Components.VRCStation)GetComponent(typeof(VRC.SDK3.Components.VRCStation));
+        _initialPosition = _station.stationEnterPlayerLocation.position;
+    }
+
+    public override void Interact()
+    {
+        Networking.SetOwner(Networking.LocalPlayer, _station.stationEnterPlayerLocation.gameObject);
+        RecalculateSeatPosition();
+        Networking.LocalPlayer.UseAttachedStation();
+    }
+
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        /*if (!player.isLocal)
+            return;
+        Networking.SetOwner(player, _station.stationEnterPlayerLocation.gameObject);
+        SendCustomEventDelayedSeconds(nameof(RecalculateSeatPosition), 0.5f);*/
+    }
+
+    public void RecalculateSeatPosition()
+    {
+        var lowerLegPos = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.LeftLowerLeg);
+        var upperLegPos = Networking.LocalPlayer.GetBonePosition(HumanBodyBones.LeftUpperLeg);
+        if (lowerLegPos == Vector3.zero || upperLegPos == Vector3.zero)
+        {
+            Debug.Log("Not adjusting seat, since we can't get the leg bone positions");
+            _station.stationEnterPlayerLocation.position = _initialPosition;
+            return;
+        }
+
+        var upperLegLength = (upperLegPos - lowerLegPos).magnitude;
+        
+        var playerPos = Networking.LocalPlayer.GetPosition();
+        
+        var ySeatEdgeOffset = lowerLegPos.y - playerPos.y;
+        var zSeatEdgeOffset = upperLegLength;
+
+        var seatEdgePosition = seatEdge.position;
+        var enterLocationScale = _station.stationEnterPlayerLocation.lossyScale;
+        _station.stationEnterPlayerLocation.position = seatEdgePosition;
+        _station.stationEnterPlayerLocation.Translate(0, -ySeatEdgeOffset*enterLocationScale.y, -zSeatEdgeOffset*enterLocationScale.z, Space.Self);
+    }
+}
